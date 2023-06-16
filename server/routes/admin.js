aea968b4ed1551cbfabd7f4c18fd3fbfb13954e3
Aw3som3ss <jabwunza@gmail.com>
2023-06-16 12:46:08 +0300

    Part 9: Add and delete Post

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -46,6 +46,25 @@ router.get('/admin', async (req, res) => {
 	}
 });
 
+router.post('/register', async (req, res) => {
+	try {
+		const { username, password } = req.body;
+		const hashedPassword = await bcrypt.hash(password, 10);
+
+		try {
+			const user = await User.create({ username, password: hashedPassword });
+			res.status(201).json({ message: 'User Created', user });
+		} catch (error) {
+			if (error.code === 11000) {
+				res.status(409).json({ message: 'User already in use' });
+			}
+			res.status(500).json({ message: 'Internal server error' });
+		}
+	} catch (error) {
+		console.log(error);
+	}
+});
+
 /*
 POST
 Admin - Check Login
@@ -85,24 +104,76 @@ router.get('/dashboard', authMiddleware, async (req, res) => {
 });
 
 /*
+GET
+Admin - Dashboard
+*/
+
+router.get('/dashboard', authMiddleware, async (req, res) => {
+	try {
+		const locals = {
+			title: 'Dashboard',
+			description: 'Simple blog created with NodeJS, Express & MongoDB.',
+		};
+
+		const data = await Post.find();
+		res.render('admin/dashboard', {
+			locals,
+			data,
+			layout: adminLayout,
+		});
+	} catch (error) {
+		console.log(error);
+	}
+});
+
+/*
 POST
 Admin - Register
 */
 
-router.post('/register', async (req, res) => {
+/*
+GET
+Admin - Create new post
+*/
+
+router.get('/add-post', authMiddleware, async (req, res) => {
 	try {
-		const { username, password } = req.body;
-		const hashedPassword = await bcrypt.hash(password, 10);
+		const locals = {
+			title: 'Add Post',
+			description: 'Simple blog created with NodeJS, Express & MongoDB.',
+		};
+
+		const data = await Post.find();
+		res.render('admin/add-post', {
+			locals,
+			layout: adminLayout,
+		});
+	} catch (error) {
+		console.log(error);
+	}
+});
+
+/*
+POST
+Admin - Create new post
+*/
+
+router.post('/add-post', authMiddleware, async (req, res) => {
+	try {
+		console.log(req.body);
 
 		try {
-			const user = await User.create({ username, password: hashedPassword });
-			res.status(201).json({ message: 'User Created', user });
+			const newPost = new Post({
+				title: req.body.title,
+				body: req.body.title,
+			});
+
+			await Post.create(newPost);
 		} catch (error) {
-			if (error.code === 11000) {
-				res.status(409).json({ message: 'User already in use' });
-			}
-			res.status(500).json({ message: 'Internal server error' });
+			console.log(error);
 		}
+
+		res.redirect('/dashboard');
 	} catch (error) {
 		console.log(error);
 	}
